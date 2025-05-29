22596c89ed2d8661a42170c8177741dc9f0626e0
Taylor Jackson <taylorj@shopavnu.com>
2025-05-28 22:33:35 -0600

    chore: fix Prettier/ESLint errors in redisClient.ts

--- backend/src/redisClient.ts
+++ backend/src/redisClient.ts
@@ -1,14 +1,16 @@
 import { createClient } from 'redis';
 
 const client = createClient({
-    username: process.env.REDIS_USERNAME || 'default',
-    password: process.env.REDIS_PASSWORD,
-    socket: {
-        host: process.env.REDIS_HOST,
-        port: Number(process.env.REDIS_PORT)
-    }
+  username: process.env.REDIS_USERNAME || 'default',
+  password: process.env.REDIS_PASSWORD,
+  socket: {
+    host: process.env.REDIS_HOST,
+    port: Number(process.env.REDIS_PORT),
+  },
 });
 
-client.on('error', err => console.log('Redis Client Error', err));
+client.on('error', (err) => {
+  console.log('Redis Client Error', err);
+});
 
 export default client;
